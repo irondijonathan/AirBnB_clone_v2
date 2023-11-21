1e74e77f785f903ca23b02117c422f4fd1433cc1
Ato Toffah <atotoffah@gmail.com>
2023-11-21 12:19:08 +0000

    4. MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- Pepares a MySQL test server for the AirBnB project
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
