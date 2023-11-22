20075ef5b2b6c2c74cbcbdb79a0fbdba560fc2b3
Ato Toffah <atotoffah@gmail.com>
2023-11-22 10:26:33 +0000

    Fixed user

--- models/user.py
+++ models/user.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from os import getenv
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    email = Column(String(128), nullable=False
+                   ) if getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    password = Column(String(128), nullable=False
+                      ) if getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    first_name = Column(String(128)
+                        ) if getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    last_name = Column(String(128)
+                       ) if getenv('HBNB_TYPE_STORAGE') == 'db' else ''
